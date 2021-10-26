cdbbad070eaaae4e67d6b0eea9b562d2c3b65a4b
Iannis Zannos <zannos@gmail.com>
2021-10-26 08:52:28 +0300

    +sclang extensions, keybindings

--- config.el
+++ config.el
@@ -73,3 +73,66 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq org-roam-directory "~/roam")
+(setq org-agenda-files '("~/org" "~/roam" "~/roam/daily"))
+(setq org-agenda-include-diary t)
+(setq diary-file "~/roam/etc/diary")
+;; (map!
+;;  ;mode dired
+;;  :desc "create symbolic link" "S" #'dired-do-symlink
+;;  )
+
+(map! :leader
+      :desc "org-mark-ring-goto" "m g o" #'org-mark-ring-goto
+      :desc "calendar" "o c" #'calendar
+      :desc "calendar" "o C" #'calc
+      :desc "find folder in project" "p F" #'projectile-find-dir
+      :desc "org mark element" "o m" #'org-mark-element
+      ;; (:prefix-map ("e" . "input encoding")
+       :desc "toggle input encoding method" "e" #'toggle-encoding
+       ;; :desc "toggle" "t" #'toggle-encoding
+       ;; :desc "english ucs" "e" #'set-encoding-ucs)
+      (:prefix-map ("k" . "scsynth")
+       :desc "boot server" "b" #'sclang-server-boot
+       :desc "start jack" "j" #'sclang-start-jack
+       :desc "server meter" "m" #'sclang-meter
+       :desc "server tree" "t" #'sclang-server-plot-tree
+       :desc "server scope audio" "s a" #'sclang-scope-audio
+       :desc "server scope control" "s c" #'sclang-scope-audio
+       :desc "quit server" "q" #'sclang-server-quit
+       :desc "reset server options" "o" #'sclang-reset-server-options)
+      (:prefix-map ("j" . "sclang")
+       :desc "switch to workspace" "w" #'sclang-switch-to-workspace
+       :desc "osc trace on" "t" #'sclang-osc-trace-on
+       :desc "osc trace off" "T" #'sclang-osc-trace-off
+       :desc "show post buffer" "p" #'sclang-show-post-buffer
+       :desc "recompile library" "l" #'sclang-recompile
+       :desc "start sclang" "s" #'sclang-start
+       :desc "stop sclang" "q" #'sclang-stop
+       :desc "run server config scripts" "c" #'sclang-server-config
+       :desc "browse builtin classes" "b" #'sclang-browse-definitions
+       :desc "browse user extension classes" "e" #'sclang-extensions-gui)
+      (:prefix-map ("l" . "latex")
+       (:prefix ("b" . "buffer")
+        ;;; TODO : replace org-journal-new-entry with real commands
+        :desc "pdflatex -> pdf" "p" #'pdflatex-compile-buffer
+        :desc "xelatex -> pdf" "x" #'xelatex-compile-buffer
+        :desc "lualatex -> pdf" "l" #'lualatex-compile-buffer
+        :desc "edit template" "e" #'org-journal-new-entry
+        :desc "reveal template path" "r" #'org-journal-new-entry
+        :desc "set template path" "t" #'org-journal-search)
+       (:prefix ("s" . "subtree")
+        :desc "pdflatex -> pdf" "p" #'org-journal-new-entry
+        :desc "xelatex -> pdf" "x" #'org-journal-new-entry
+        :desc "lualatex -> pdf" "l" #'org-journal-new-entry
+        :desc "edit template" "e" #'org-journal-new-entry
+        :desc "reveal template path" "r" #'org-journal-new-entry
+        :desc "set template path" "t" #'org-journal-search))
+      (:prefix ("b" . "buffer")
+        :desc "pdflatex -> pdf" "p" #'org-journal-new-entry
+        :desc "xelatex -> pdf" "x" #'org-journal-new-entry
+        :desc "lualatex -> pdf" "l" #'org-journal-new-entry
+        :desc "edit template" "e" #'org-journal-new-entry
+        :desc "reveal template path" "r" #'org-journal-new-entry
+        :desc "set template path" "t" #'org-journal-search))
