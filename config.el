021129ca8dc9c29df15292ca2a17c5ddbd231c39
Iannis Zannos <zannos@gmail.com>
2021-11-20 11:07:32 +0200

    projectile use commander, undo-tree

--- config.el
+++ config.el
@@ -19,7 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;; (setq doom-font (font-spec :family "Iosevka SS04" :size 24 :weight 'light
+;;                            )
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
@@ -123,7 +124,10 @@
        :desc "toggle input encoding method" "e" #'toggle-encoding
        ;; :desc "toggle" "t" #'toggle-encoding
        ;; :desc "english ucs" "e" #'set-encoding-ucs)
-      (:prefix-map ("k" . "scsynth")
+       (:prefix-map ("v" . "various iani")
+       :desc "projectile commander" "p" #'projectile-use-commander
+       :desc "undo tree" "t" #'undo-tree-mode)
+       (:prefix-map ("k" . "scsynth")
        :desc "boot server" "b" #'sclang-server-boot
        :desc "start jack" "j" #'sclang-start-jack
        :desc "server meter" "m" #'sclang-meter
--- postload/08_projectile_user_commander.el
+++
@@ -1,2 +0,0 @@
-;;; postload/08_projectile_user_commander.el -*- lexical-binding: t; -*-
-(setq projectile-switch-project-action #'projectile-commander)
